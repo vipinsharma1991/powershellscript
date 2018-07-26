e40c641de6ee0ee616e61244f567963418a8024f
vipinsharma1991 <41724149+vipinsharma1991@users.noreply.github.com>
2018-07-26 15:39:14 +0800

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,11 +1,16 @@
+<powershell>
 
-Start-Process -FilePath "winrm" -ArgumentList "quickconfig -q"
-Start-Process -FilePath "winrm" -ArgumentList "set winrm/config/service @{AllowUnencrypted=`"true`"}" -Wait
-Start-Process -FilePath "winrm" -ArgumentList "set winrm/config/service/auth @{Basic=`"true`"}" -Wait
-Start-Process -FilePath "winrm" -ArgumentList "set winrm/config @{MaxTimeoutms=`"1900000`"}"
-netsh advfirewall firewall add rule name=\"WinRM 5985\" protocol=TCP dir=in localport=5985 action=allow
-netsh advfirewall firewall add rule name=\"WinRM 5986\" protocol=TCP dir=in localport=5986 action=allow
+winrm quickconfig -q
+winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="300"}'
+winrm set winrm/config '@{MaxTimeoutms="1800000"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+winrm set winrm/config/service/auth '@{Basic="true"}'
+
+netsh advfirewall firewall add rule name="WinRM 5985" protocol=TCP dir=in localport=5985 action=allow
+netsh advfirewall firewall add rule name="WinRM 5986" protocol=TCP dir=in localport=5986 action=allow
+
 net stop winrm
 sc config winrm start= auto
-powershell net start winrm
+net start winrm
 
+</powershell>
