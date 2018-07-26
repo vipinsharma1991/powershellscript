c6c475519a1d4a55894e726eb3926de734cb538d
vipinsharma1991 <41724149+vipinsharma1991@users.noreply.github.com>
2018-07-26 22:04:55 +0800

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,5 +1,7 @@
+wmic useraccount where "name='vipin.sharma'" set PasswordExpires=FALSE
+
 winrm quickconfig -q
-winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="300"}'
+winrm set winrm/config/winrs '@{MaxMemoryPerShellMB="4096"}'
 winrm set winrm/config '@{MaxTimeoutms="1800000"}'
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
@@ -8,5 +10,6 @@ netsh advfirewall firewall add rule name="WinRM 5985" protocol=TCP dir=in localp
 netsh advfirewall firewall add rule name="WinRM 5986" protocol=TCP dir=in localport=5986 action=allow
 
 net stop winrm
-sc config winrm start= auto
+sc config winrm start=auto
 net start winrm
+Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine
