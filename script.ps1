a10a641225470c812dfb4326626d306045bdabbc
vipinsharma1991 <41724149+vipinsharma1991@users.noreply.github.com>
2018-07-26 15:10:31 +0800

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,11 @@
+
 Start-Process -FilePath "winrm" -ArgumentList "quickconfig -q"
 Start-Process -FilePath "winrm" -ArgumentList "set winrm/config/service @{AllowUnencrypted=`"true`"}" -Wait
 Start-Process -FilePath "winrm" -ArgumentList "set winrm/config/service/auth @{Basic=`"true`"}" -Wait
 Start-Process -FilePath "winrm" -ArgumentList "set winrm/config @{MaxTimeoutms=`"1900000`"}"
+netsh advfirewall firewall add rule name=\"WinRM 5985\" protocol=TCP dir=in localport=5985 action=allow
+netsh advfirewall firewall add rule name=\"WinRM 5986\" protocol=TCP dir=in localport=5986 action=allow
+net stop winrm
+sc config winrm start= auto
+powershell net start winrm
+
