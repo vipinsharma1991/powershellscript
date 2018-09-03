451630176fe7bd55cbdf3f0f44b401cf989ef0c7
vipinsharma1991 <41724149+vipinsharma1991@users.noreply.github.com>
2018-09-03 20:46:54 +0800

    Update Script-windows10.ps1

--- Script-windows10.ps1
+++ Script-windows10.ps1
@@ -10,22 +10,6 @@ netsh advfirewall firewall add rule name="WinRM 5985" protocol=TCP dir=in localp
 netsh advfirewall firewall add rule name="WinRM 5986" protocol=TCP dir=in localport=5986 action=allow
 
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine
-# Get the public networks
-$PubNets = Get-NetConnectionProfile -NetworkCategory Public -ErrorAction SilentlyContinue 
-
-# Set the profile to private
-foreach ($PubNet in $PubNets) {
-    Set-NetConnectionProfile -InterfaceIndex $PubNet.InterfaceIndex -NetworkCategory Private
-}
-
-# Configure winrm
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-
-# Restore network categories
-foreach ($PubNet in $PubNets) {
-    Set-NetConnectionProfile -InterfaceIndex $PubNet.InterfaceIndex -NetworkCategory Public
-}
-
 net stop winrm
 sc config winrm start=auto
 net start winrm
