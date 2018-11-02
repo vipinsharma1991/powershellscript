75f8c1493b643a97a10b5136bfa012298d0c95b6
vipinsharma1991 <41724149+vipinsharma1991@users.noreply.github.com>
2018-11-02 18:53:09 +0800

    Update Scriptupdated.sh

--- Scriptupdated.sh
+++ Scriptupdated.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+#!/usr/bin/expect
 sudo -i
 echo 'pconnectadmin ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 useradd -m $1 -p "Passw0rd@123"
-echo -e "Passw0rd@123\nPassw0rd@123" | passwd $1
+echo -e "Passw0rd@123\nPassw0rd@123" | passwd $1;
