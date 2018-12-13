15daac9afbda99cac83fa8df878b267e49f266d7
vipinsharma1991 <41724149+vipinsharma1991@users.noreply.github.com>
2018-12-13 17:14:21 +0800

    Update Scriptupdated.sh

--- Scriptupdated.sh
+++ Scriptupdated.sh
@@ -4,3 +4,5 @@ sudo -i
 echo 'pconnectadmin ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 useradd -m $1 -p "Passw0rd@123";
 echo "$1:Passw0rd@123" | chpasswd
+sudo unlink /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Singapore /etc/localtime
