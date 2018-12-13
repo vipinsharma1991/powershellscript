a6a82f24d2e1d90a550f8c3d1f54bfac266208b8
vipinsharma1991 <41724149+vipinsharma1991@users.noreply.github.com>
2018-12-13 17:15:08 +0800

    Update DevScript.sh

--- DevScript.sh
+++ DevScript.sh
@@ -3,3 +3,5 @@ sudo -i
 echo 'pconnectadmin ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 useradd -m $1 -p $2
 echo "$1:$2" | chpasswd
+sudo unlink /etc/localtime
+sudo ln -s /usr/share/zoneinfo/Singapore /etc/localtime
