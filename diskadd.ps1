1918b22520f0287bce012b182c24a77fe069ab38
vipinsharma1991 <41724149+vipinsharma1991@users.noreply.github.com>
2019-02-26 19:28:11 +0800

    Update diskadd.ps1

--- diskadd.ps1
+++ diskadd.ps1
@@ -1,1 +1,2 @@
-Get-Disk | Where partitionstyle -eq ‘raw’ | Initialize-Disk -PartitionStyle MBR -PassThru | New-Partition -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel “disk2” -Confirm:$false
+Initialize-Disk -Number 2 -PartitionStyle MBR -PassThru
+New-Partition -DiskNumber 2 -AssignDriveLetter –UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel “disk2” -Confirm:$false
