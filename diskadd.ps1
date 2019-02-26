916ff46a51415ebea827011710f9e236d814cf49
vipinsharma1991 <41724149+vipinsharma1991@users.noreply.github.com>
2019-02-26 19:41:37 +0800

    Update diskadd.ps1

--- diskadd.ps1
+++ diskadd.ps1
@@ -1,2 +1,2 @@
 Initialize-Disk -Number 2 -PartitionStyle MBR -PassThru
-New-Partition -DiskNumber 2 -AssignDriveLetter –UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel “disk2” -Confirm:$false
+New-Partition -DiskNumber 2 -AssignDriveLetter -UseMaximumSize | Format-Volume -FileSystem NTFS -NewFileSystemLabel “disk2” -Confirm:$false
