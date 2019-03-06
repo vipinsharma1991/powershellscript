7c12b28bfb3f53ff6577bd23f7327742728771f3
vipinsharma1991 <41724149+vipinsharma1991@users.noreply.github.com>
2019-03-06 18:45:40 +0800

    Update DevScript.sh

--- DevScript.sh
+++ DevScript.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 sudo -i
-echo 'pconnectadmin ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
+echo 'CodexAdmin ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 useradd -m $1 -p $2
 echo "$1:$2" | chpasswd
 sudo unlink /etc/localtime
