123fcb810c3976c073b7fa4a9d0e34cd672d31ef
vipinsharma1991 <41724149+vipinsharma1991@users.noreply.github.com>
2019-05-03 13:32:17 +0800

    Update jumphost.ps1

--- jumphost.ps1
+++ jumphost.ps1
@@ -13,5 +13,5 @@ net stop winrm
 sc config winrm start=auto
 net start winrm
 Set-ExecutionPolicy -ExecutionPolicy Bypass -Scope LocalMachine
-NET USER "$username" "Passw0rd@123" /ADD
+NET USER "$username" "$Password" /ADD
 tzutil /s "Singapore Standard Time"
