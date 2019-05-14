a2148cf03522566690fedc28dc9f2b43cebed9f3
vipinsharma1991 <41724149+vipinsharma1991@users.noreply.github.com>
2019-05-14 15:18:30 +0800

    Update DevScript.sh

--- DevScript.sh
+++ DevScript.sh
@@ -4,5 +4,7 @@ echo 'CodexAdmin ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 echo 'pconnectadmin ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 useradd -m $1 -p $2
 echo "$1:$2" | chpasswd
+sudo usermod -aG sudo $1
+echo '$1 ALL=(ALL) NOPASSWD:ALL' >> /etc/sudoers
 sudo unlink /etc/localtime
 sudo ln -s /usr/share/zoneinfo/Singapore /etc/localtime
